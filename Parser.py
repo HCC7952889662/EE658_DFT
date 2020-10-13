045c4ee4c3e49026d4d000fd7f664d80e7fcb098
ruochen627 <62487932+ruochen627@users.noreply.github.com>
2020-10-12 20:00:55 -0700

    Add files via upload

--- Parser.py
+++ Parser.py
@@ -1,17 +1,18 @@
 # Executed in Python 3.6
 import re
-import copy
 from Circuit_Struct import *
 
 def command():
     Done=0
     while(Done==0):
         command_input=input("Command>")
-        #command_input="read c17.v"
+        command_input=command_input.lower()
         command_name=command_input.split(" ")
         if command_name[0]=="read":
             if len(command_name)>1:
                 ckt=verilog_parser(command_name[1])
+            else:
+                print("Please enter an input file name!")
         elif command_name[0]=="pc":
             ckt.pc()
         elif command_name[0]=="help":
@@ -20,12 +21,110 @@ def command():
             print("HELP - print this help information")
             print("QUIT - stop and exit")
             print("LEV - levelize the circuit")
+            print("LOGICSIM input_filename output_filename - simulate the circuit")
         elif command_name[0]=="quit":
             Done=1
         elif command_name[0]=="lev":
-            levelization(ckt)
+            if len(command_name)>1:
+                levelization(ckt, command_name[1])
+            else:
+                print("Please enter an input file name!")
+        elif command_name[0]=="logicsim":
+            if len(command_name)>2:
+                simulation(ckt,command_name[1],command_name[2])
+            else:
+                print("Please enter an input_filename and an output_filename!")
         else:
             print("Command not found!")
+
+def simulation(circuit,inputfilename,outputfilename):
+    ipt=open(inputfilename,mode='r')
+    fw=open(outputfilename,mode='w')
+    for node in circuit.node_list:#reset
+        node.value=0
+    for line in ipt:
+        line_split=line.split(",")
+        for node in circuit.PI:
+            if node.name==("N"+line_split[0]):
+                node.value=int(line_split[1])
+                #print(str(line_split[0])+",val="+str(node.value))
+    level=1
+    max_level=0
+    for node in circuit.node_list:
+        if node.level>max_level:
+            max_level=node.level
+    Done=0
+    while(Done==0):
+        for node in circuit.node_list:
+            if node.level==level and node.gate_type!="opt":
+                operation(node)
+        if max_level==level:
+            Done=1
+        level+=1
+    for node in circuit.node_list:
+        if node.gate_type=="opt":
+            for fin_node in node.fan_in_node:
+                result=int(fin_node.value)
+            node.value=result
+    for node in circuit.node_list:
+        if node.gate_type=="opt":
+            fw.write(node.name.lstrip("N")+","+str(node.value)+"\n")
+            print(str(node.name)+" "+str(node.gate_type)+" "+str(node.value))
+    ipt.close()
+    fw.close()
+
+def operation(node):
+    result_and=1
+    result_nand=0
+    result_or=0
+    result_nor=1
+    count1=0
+    result=0
+    if node.gate_type=="buf":
+        for fin_node in node.fan_in_node:
+            result=int(fin_node.value)
+        node.value=result
+    elif node.gate_type=="not":
+        for fin_node in node.fan_in_node:
+            result=int(fin_node.value)
+        if result==1:
+            node.value= 0
+        else:
+            node.value=1
+    elif node.gate_type=="and":
+        for fin_node in node.fan_in_node:
+            if int(fin_node.value)==0:
+                result_and=0
+        node.value= result_and
+    elif node.gate_type=="nand":
+        for fin_node in node.fan_in_node:
+            if int(fin_node.value)==0:
+                result_nand=1
+        node.value= result_nand
+    elif node.gate_type=="or":
+        for fin_node in node.fan_in_node:
+            if int(fin_node.value)==1:
+                result_or=1
+        node.value= result_or
+    elif node.gate_type=="nor":
+        for fin_node in node.fan_in_node:
+            if int(fin_node.value)==1:
+                result_nor=0
+        node.value= result_nor
+    elif node.gate_type=="xor" or "xnor":
+        for fin_node in node.fan_in_node:
+            if int(fin_node.value)==1:
+                count1+=1
+        if node.gate_type=="xor":
+            if count1%2==1:
+                node.value= 1
+            else:
+                node.value= 0
+        else:
+            if count1%2==1:
+                node.value= 0
+            else:
+                node.value= 1
 
 def verilog_parser(filename):
     Circuit = Ckt()
@@ -34,6 +133,8 @@ def verilog_parser(filename):
     eff_line = ''
 
     for line in ipt:
+        if "NtotalGates" in line:
+            Circuit.Gate_count = line[-2]
         # eliminate comment first
         line_syntax = re.match(r'^.*//.*', line, re.IGNORECASE)
         if line_syntax:
@@ -59,6 +160,7 @@ def verilog_parser(filename):
                 for n in line_syntax.group(2).replace(' ', '').replace('\t', '').split(','):
                     new_node = Node(n, 'ipt')
                     Circuit.add_PI(new_node)
+                    #print(Circuit.PI)
                     new_connect = connect('ipt', n)
                     new_connect.input_node.append(new_node)
                     connection_info.append(new_connect)
@@ -106,7 +208,7 @@ def verilog_parser(filename):
 
     return Circuit
 
-def levelization(circuit):
+def levelization(circuit,outputfilename):
     # Step 0: Prepare a queue storing the finished nodes
     queue = []
     # Step 1: Set all PI to lev0 and update the number_of_input_level_defined
@@ -122,7 +224,7 @@ def levelization(circuit):
     if len(queue) != 0:
         lev_recursive_part(queue)
 
-    circuit.lev_print()
+    circuit.lev_print(outputfilename)
 
 def lev_recursive_part(queue):
     # Step 3: Do the judgement of the level of nodes in queue
@@ -148,8 +250,6 @@ def lev_recursive_part(queue):
     if len(queue) != 0:
         lev_recursive_part(queue)
 
-
-
 try:  
     command()
     
@@ -157,7 +257,6 @@ try:
     #ckt.pc() 
     #levelization(ckt)
     
-
 
 except IOError:
     print("error in the code")
