ddd6581c1bc558c525a930dee187a81329a68348
Ting-Yu <72479248+TingYu0924@users.noreply.github.com>
2020-10-21 11:48:56 +0800

    command

--- Parser.py
+++ Parser.py
@@ -10,7 +10,8 @@ def command():
         command_name=command_input.split(" ")
         if command_name[0]=="read":
             if len(command_name)>1:
-                ckt=verilog_parser(command_name[1])
+                ckt=Circuit(command_name[1])
+                #ckt.verilog_parser(command_name[1])
             else:
                 print("Please enter an input file name!")
         elif command_name[0]=="pc":
@@ -26,14 +27,24 @@ def command():
             Done=1
         elif command_name[0]=="lev":
             if len(command_name)>1:
-                levelization(ckt, command_name[1])
+                ckt.levelization(command_name[1])
             else:
-                print("Please enter an input file name!")
+                print("Please enter an output file name!")
         elif command_name[0]=="logicsim":
             if len(command_name)>2:
                 simulation(ckt,command_name[1],command_name[2])
             else:
                 print("Please enter an input_filename and an output_filename!")
+        elif command_name[0] == 'check':
+            if len(command_name) > 2:
+                file_check(command_name[1], command_name[2])
+            else:
+                print("Please enter an input_filename and an output_filename!")
+        elif command_name[0] == 'tb_gen':
+            if len(command_name) > 1:
+                ckt.testbench_generator(command_name[1])
+            else:
+                print("Please enter how many test vectors you want to generate!")
         else:
             print("Command not found!")
 
@@ -125,6 +136,38 @@ def operation(node):
                 node.value= 0
             else:
                 node.value= 1
+
+def file_check(file1,file2):
+    origin_output_file = open(file1, "r+")
+    # print('reading', new_output_file)
+    new_output_file = open(file2, "r+")
+    number_of_line = 1
+    origin_line = origin_output_file.readline()
+    new_line = new_output_file.readline()
+    flag = 1
+    if len(origin_line) == 0:
+        print("original file is empty!")
+        flag = 0
+    if len(new_line) == 0:
+        print("new file is empty!")
+        flag = 0
+    if origin_line is not None and new_line is not None:
+        while origin_line:
+            if origin_line != new_line:
+                print('file different! different line is #', number_of_line)
+                flag = 0
+
+            else:
+                flag = 1
+            origin_line = origin_output_file.readline()
+            new_line = new_output_file.readline()
+            number_of_line += 1
+    if flag == 1:
+        print('result are the same')
+    else:
+        print("result are not same!")
+    origin_output_file.close()
+    new_output_file.close()
 
 try:
     command()
